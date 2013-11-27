867169cb327527756d96b2d0dbb4924adb52b408
Maxime SCHMITT <maxime.schmitt91@gmail.com>
2013-11-27 14:39:56 +0100

    struct to compute time between two calls of function

---
+++ include/timeuh.h
@@ -0,0 +1,19 @@
+#ifndef __TIMEUH_H
+#define __TIMEUH_H
+#include <time.h>
+
+typedef struct {
+    struct timespec begin, end;
+} time_counter;
+
+inline int get_begin_time(time_counter *tc, clockid_t clk_id){
+    return clock_gettime(clk_id, &tc->begin);
+}
+
+inline int get_end_time(time_counter *tc, clockid_t clk_id){
+    return clock_gettime(clk_id, &tc->end);
+}
+
+struct timespec time_diff(const time_counter *tc);
+
+#endif // __TIMEUH_H
---
+++ src/timeuh.c
@@ -0,0 +1,12 @@
+#include <timeuh.h>
+
+struct timespec time_diff(const time_counter *tc){
+    struct timespec tp;
+    tp.tv_sec = tc->end.tv_sec - tc->begin.tv_sec;
+    tp.tv_nsec = tc->end.tv_nsec - tc->begin.tv_nsec;
+    if(tp.tv_nsec<0){
+        tp.tv_sec -= 1;
+        tp.tv_nsec += 1000000000;
+    }
+    return tp;
+}
