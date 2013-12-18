b0b5519433e8f2963fbf2b4ef05872759454958d
sovereign <sovereign@syllo.umbrellacorp>
2013-12-18 16:04:20 +0100

    Ping ICMP Fonctionnel

--- makefile
+++ makefile
@@ -1,17 +1,18 @@
-CFLAGS = -Wall -std=gnu99 -pedantic -Werror -W -Wextra -Wmissing-declarations -Wmissing-prototypes -Wreduntand-decls -Wshadow -Wbad-function-cast -Wcast-qual
+CFLAGS = -Wall -std=gnu99 -pedantic -Werror -W -Wextra -Wmissing-declarations -Wmissing-prototypes -Wredundant-decls -Wshadow -Wbad-function-cast -Wcast-qual
 OPATH = obj/
 IFLAGS = -Iinclude/
+LIBFLAGS = -lrt -pthread
 
 vpath %.c src/
 vpath %.h include/
 vpath %.o obj/
 
-ping : ping.o | bin
-	gcc $(CFLAGS) -o ping $(OPATH)ping.o
+ping : ping.o timeuh.o | bin
+	gcc $(CFLAGS) -o ping $(OPATH)ping.o $(OPATH)timeuh.o $(LIBFLAGS)
 	mv $@ bin/
 
 ping.o : ping.c ping.h
-traceroute.o :
+timeuh.o : timeuh.c timeuh.h
 
 %.o : | obj
 	gcc $(CFLAGS) -c $< $(IFLAGS)
--- src/ping.c
+++ src/ping.c
@@ -14,11 +14,14 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <string.h>
+#include <limits.h>
 
 #define MAXPACKET 4096
 
@@ -27,7 +30,7 @@ void (*pinger)(void);
 char* hostname;
 int pid;
 int sockfd;
-long unsigned int timeMin=0;
+long unsigned int timeMin=ULONG_MAX;
 long unsigned int timeMax=0;
 long unsigned int timeOverall=0;
 long unsigned int nbrReceive=0;
@@ -37,8 +40,12 @@ unsigned int sizeData;
 struct sockaddr_in destination;
 pthread_t threadPinger;
 unsigned char buffer[MAXPACKET];
+char nameDest[INET6_ADDRSTRLEN];
 
 void * pingou (void * time){
+	sigset_t mask;
+	sigfillset(&mask);
+	pthread_sigmask(SIG_BLOCK, &mask, NULL);
 	struct timespec* timer=(struct timespec*) time;
 	for(;;){
 		if(limitePing!=0 && nbrSend>=limitePing){
@@ -53,8 +60,9 @@ void * pingou (void * time){
 	}
 }
 
-void pingerICMP(){
-	int i;
+void pingerICMP(void){
+	size_t i;
+	int nbs;
 	unsigned char packet[MAXPACKET];
 	struct icmp *icmpPacket=(struct icmp*) packet;
 	struct timespec *time=(struct timespec*) &packet[8]; // partie data de icmp
@@ -70,13 +78,13 @@ void pingerICMP(){
 		*data++=i;
 	}
 	clock_gettime(CLOCK_REALTIME, time);
-	icmpPacket->icmp_cksum=checksum(icmpPacket, 8+sizeData);
-	i=sendto(sockfd, packet, 8+sizeData, 0, (struct sockaddr*) &destination, sizeof(struct sockaddr));
+	icmpPacket->icmp_cksum=checksum((unsigned short*) icmpPacket, 8+sizeData);
+	nbs=sendto(sockfd, packet, 8+sizeData, 0, (struct sockaddr*) &destination, sizeof(struct sockaddr));
 	
-	if(i<0 || i< 8+sizeData){
-		if(i<0)
+	if(nbs<0 || (unsigned int) nbs< 8+sizeData){
+		if(nbs<0)
 			perror("sendto :");
-		fprintf(stderr, "ping : sendto %s %d chars, achieve %d\n", hostname, 8+sizeData, i);
+		fprintf(stderr, "ping : sendto %s %d chars, achieve %d\n", hostname, 8+sizeData, nbs);
 		fflush(stderr);
 	}
 	nbrSend++;
@@ -102,16 +110,18 @@ int checksum(unsigned short* icmp, int totalLength){
 void sigIntAction(int signum){
 	pthread_cancel(threadPinger);
 	fflush(stdout);
+	printf("Signal %d received\n", signum);
 	printf("************** STATISTICS FOR %s PINGING **************\n", hostname);
 	printf("%ld packets sent, ", nbrSend);
 	printf("%ld packets received, ", nbrReceive);
-	if(nbrSend>0)
+	if(nbrSend>0){
 		if(nbrReceive>nbrSend)
 			printf("vodoo magic happened, we received more packets than we sent!\n");
 		else
-			printf("%d%% packet loss\n", (nbrSend-nbrReceive)*100/nbrSend);
+			printf("%lu%% packet loss\n", (nbrSend-nbrReceive)*100/nbrSend);
+	}
 	if(nbrReceive>0)
-		printf("round-trip time (ms) min/max/avg =>  %d/%d/%d\n", timeMin, timeMax, timeOverall/nbrReceive);
+		printf("round-trip time (ms) min/max/avg =>  %lu/%lu/%lu\n", timeMin, timeMax, timeOverall/nbrReceive);
 	fflush(stdout);
 	exit(EXIT_SUCCESS);
 }
@@ -119,12 +129,11 @@ void sigIntAction(int signum){
 void lirePacketICMP(unsigned char* buf, int size, struct sockaddr_in* doctorWho){
 	struct ip* ip;
 	struct icmp* icmpPacket;
-	long *lp =(long*) buf;
-	int i;
 	struct timespec tnow;
 	struct timespec* tbefore;
 	struct timespec diff;
-	int ipheaderlen, timems;
+	int ipheaderlen;
+	unsigned int timems;
 	
 	clock_gettime(CLOCK_REALTIME, &tnow);
 	ip=(struct ip*) buf;
@@ -158,6 +167,14 @@ int main(int argc, char** argv){
 	struct sigaction siga;
 	struct timespec timetowait;
 	struct sockaddr_in from;
+	struct addrinfo wantedAddr;
+	struct addrinfo *to;
+	struct addrinfo *parseAddr;
+	
+	if(argc!=2){
+		printf("Utilisation : %s -option1 -option2 ... adresse/url\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 	
 	pid=getpid();
 	siga.sa_handler=sigIntAction;
@@ -165,6 +182,26 @@ int main(int argc, char** argv){
 	siga.sa_flags=0;
 	sigaction(SIGINT, &siga, NULL);
 	
+	hostname=argv[1];
+	
+	memset(&wantedAddr, 0, sizeof(struct addrinfo));
+	wantedAddr.ai_family=AF_INET;
+	wantedAddr.ai_socktype=SOCK_RAW;
+	wantedAddr.ai_protocol=IPPROTO_ICMP;
+	getaddrinfo(hostname, NULL, &wantedAddr, &to);
+	
+	for(parseAddr=to; parseAddr!=NULL; parseAddr=parseAddr->ai_next){
+		sockfd=socket(parseAddr->ai_family, parseAddr->ai_socktype, parseAddr->ai_protocol);
+		if(sockfd!=-1){
+			memcpy(&destination, parseAddr->ai_addr, sizeof(struct sockaddr_in));
+			break;
+		}
+	}
+	if(parseAddr==NULL){ // aucune addresse trouvée
+		printf("%s, unknown host %s\n", argv[0], hostname);
+		exit(EXIT_FAILURE);
+	}
+	freeaddrinfo(to);
 	// faire les actions stdin
 	//****************** A CHANGER AVEC LES OPTIONS TOUT CA ***********************
 	timetowait.tv_sec=1;
@@ -173,15 +210,16 @@ int main(int argc, char** argv){
 	sizeData=1024;
 	// ******************* JUSQU'ICI ***********************************************
 	pthread_create(&threadPinger, NULL, pingou, &timetowait);
+	inet_ntop(destination.sin_family, &destination.sin_addr, nameDest, INET6_ADDRSTRLEN);
+	printf("Start pinging %s (%s)\n", hostname, nameDest);
 	for(;;){
-		int doctorWhoLength=sizeof(from);
+		socklen_t doctorWhoLength=sizeof(from);
 		int nbrecv;
 		if((nbrecv=recvfrom(sockfd, buffer, MAXPACKET, 0, (struct sockaddr*) &from, &doctorWhoLength))<0){
 			perror("recvfrom :");
 			continue;
 		}
-		lirePacketICMP(&buffer, nbrecv, &from);
-		
+		lirePacketICMP(buffer, nbrecv, &from);
 	}
 	return EXIT_SUCCESS;
 }
